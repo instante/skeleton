0a1fe3d481a3a674731cb0e765e2467779828ff0
Richard Ejem <richard@ejem.cz>
2015-10-06 17:47:09 +0200

    fixed windows test runner command

--- bin/run-tests.cmd
+++ bin/run-tests.cmd
@@ -1,7 +1,8 @@
 pushd "%~dp0\.."
 
 copy .\tests\php-win.ini .\tests\php.ini
-echo "" >> .\tests\php.ini # empty line
+rem add empty line in case of newline wasn't at the end
+echo.  >> .\tests\php.ini 
 php -r "echo 'extension_dir=' . ini_get('extension_dir');" >> .\tests\php.ini
 
 call .\libs\composer\bin\tester .\tests -p php -c .\tests

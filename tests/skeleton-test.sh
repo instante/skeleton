661f27b67bd6f4c530501616bc102c8920109517
Richard Ejem <richard@ejem.cz>
2015-10-29 11:26:16 +0100

    fixed test script

--- tests/skeleton-test.sh
+++ tests/skeleton-test.sh
@@ -6,14 +6,16 @@ PHP_EXT=`php -r "echo ini_get('extension_dir');"`
 echo "" >> ./tests/php.ini # empty line
 echo "extension_dir=$PHP_EXT" >> ./tests/php.ini
 
-php ./bin/deployment/init-project.php < "test@doe.com\nfoo/bar\ndescription\nlic\nver\nauthorname\nauthormail\n"
+echo -e "test@doe.com\nfoo/bar\ndescription\nlic\nver\nauthorname\nauthormail\n" | php ./bin/deployment/init-project.php
 
 if [ $? -ne 0 ]; then
+    echo "failed executing init-project.php: returned exitcode $?"
     popd
     exit 1
 fi
 
 if [ `cat app/config/default.neon | grep "test@doe.com" | wc -l` != 1 ]; then
+    echo "failed: app/config/default.neon does not contain set e-mail"
     popd
     exit 2
 fi

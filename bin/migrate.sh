d53e097f0aa841e43a45785b3b10362d916a1d9d
Richard Ejem <richard@ejem.cz>
2016-01-15 10:17:49 +0100

    fixed present migrations check

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pushd "$(dirname "$0")/.."
 
-if [ -f migrations/Version*.php ]; then
+if [ `ls -la migrations/Version*.php | wc -l` -gt 0 ]; then
     echo "executing migrations..."
     php www/index.php migrations:migrate --no-interaction
 else

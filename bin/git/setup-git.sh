8b50c3942f8288b284aa982fc484de673676842d
Richard Ejem <richard@ejem.cz>
2016-06-21 14:57:22 +0200

    removed old git recompliation hooks

--- bin/git/setup-git.sh
+++ bin/git/setup-git.sh
@@ -4,9 +4,4 @@ pushd "$(dirname "$0")/../.."
 # disable auto crlf conversion
 git config core.autocrlf false
 
-# install hooks to recompile grunt after merge or rebase if needed
-git config merge.dummize.driver "bin/git/dummize.sh %%A"
-cp bin/git/post-merge.hook .git/hooks/post-merge
-cp bin/git/post-rewrite.hook .git/hooks/post-rewrite
-
 popd

6edec486bf9dcd855394f00b0626ed311d6c6c93
Richard Ejem <richard@ejem.cz>
2016-09-06 17:41:19 +0200

    improved deploy script to use global bower and grunt if available or install them locally (or skip frontend compilation completely if npm is not available)

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -3,7 +3,7 @@
 # this script is used to deploy changes from repository (the project has to be configured locally before).
 # add any re-deploy build steps of your application there.
 
-pushd "$(dirname "$0")/.."
+pushd "$(dirname "$0")/.." > /dev/null
 
 cp app/.maintenance.php www/.maintenance.php
 
@@ -12,11 +12,27 @@ composer install
 bin/purge-cache.sh
 bin/migrate.sh
 
-cd frontend
-npm install
-node_modules/bower/bin/bower install
-node_modules/grunt-cli/bin/grunt dist
+if command -v npm > /dev/null; then
+    cd frontend
+    npm install
+    if command -v bower > /dev/null; then
+        bower install
+    else
+        npm install bower
+        node_modules/bower/bin/bower install
+    fi
+
+    if command -v grunt > /dev/null; then
+        grunt dist
+    else
+        npm install grunt-cli
+        node_modules/grunt-cli/bin/grunt dist
+    fi
+    cd ..
+else
+    echo "npm not installed, skipping frontend compilation"
+fi
 
 rm -f www/.maintenance.php
 
-popd
+popd > /dev/null

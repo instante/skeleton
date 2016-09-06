6ebb49231ed8194e841afcbc0eef7423d3d4adf4
Richard Ejem <richard@ejem.cz>
2016-09-06 17:07:32 +0200

    check PHP version in index.php

--- www/index.php
+++ www/index.php
@@ -1,4 +1,9 @@
 <?php
+if (PHP_VERSION_ID < 50600) {
+    // Application may crash on parse errors on insufficient PHP version.
+    // Rather give user-friendly error message here.
+    die ('Instante needs PHP version 5.6+');
+}
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require '.maintenance.php';
 // Let bootstrap create Dependency Injection container.

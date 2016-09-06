192ba9935c57d64fc45251c9bea6547bdd17db07
Richard Ejem <richard@ejem.cz>
2016-09-06 17:28:38 +0200

    moved git commands to simpler location

--- bin/git/merge-current-into-develop.sh
+++ bin/git/merge-current-into-develop.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname "$0")/../../.." >/dev/null
+pushd "$(dirname "$0")/../.." >/dev/null
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 git checkout develop
 git pull
-git checkout "$branch"
-git rebase develop
+git merge --no-ff --no-edit "$branch"
 
 popd >/dev/null
+
--- bin/git/rebase-current-on-develop.sh
+++ bin/git/rebase-current-on-develop.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname "$0")/../../.." >/dev/null
+pushd "$(dirname "$0")/../.." >/dev/null
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 git checkout develop
 git pull
-git merge --no-ff --no-edit "$branch"
+git checkout "$branch"
+git rebase develop
 
 popd >/dev/null
-

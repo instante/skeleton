c50b8f4a509be7e8cfe49ab7437260f57fb0d52f
Richard Ejem <richard@ejem.cz>
2016-09-20 09:46:48 +0200

    add all changes to working tree before checkout

--- bin/checkout.sh
+++ bin/checkout.sh
@@ -2,4 +2,5 @@
 
 dir=${1-instante-app}
 mkdir "$dir"
+git --git-dir "$(dirname "$0")/../.git" --work-tree "$(dirname "$0")/../" add -A
 git --git-dir "$(dirname "$0")/../.git" --work-tree "$dir" checkout-index -a -f

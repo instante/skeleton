6a10b5294985d92f8ad7ae93bf96a420b6e54c81
Richard Ejem <richard@ejem.cz>
2016-09-22 17:30:19 +0200

    checkout script symlinks vendor

--- bin/checkout.sh
+++ bin/checkout.sh
@@ -4,3 +4,7 @@ dir=${1-instante-app}
 mkdir "$dir"
 git --git-dir "$(dirname "$0")/../.git" --work-tree "$(dirname "$0")/../" add -A
 git --git-dir "$(dirname "$0")/../.git" --work-tree "$dir" checkout-index -a -f
+
+ABS_VENDOR=`cd "$(dirname "$0")/../vendor"; pwd`
+rm -rf "$dir/vendor"
+ln -s "$ABS_VENDOR" "$dir"

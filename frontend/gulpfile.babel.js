a8ce6e3b8b62ec042d519b27540516f70dfec3e4
Honza Škovran <honza@skovran.net>
2016-10-17 09:36:50 +0200

    added rjs support

--- frontend/gulpfile.babel.js
+++ frontend/gulpfile.babel.js
@@ -2,25 +2,31 @@
 
 import fs from 'fs';
 import path from 'path';
-import merge from 'merge-stream';
 import gulp from 'gulp';
 import concat from 'gulp-concat';
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify';
 import Dependo from 'dependo';
 import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
 import less from 'gulp-less';
 import cssnano from 'gulp-cssnano';
+import rjs from 'gulp-requirejs-optimize';
 
 const src = {
     scripts: 'src/js',
+    views: 'src/js/views',
     less: 'src/less'
 };
 
 const dist = {
     scripts: '../www/js',
     styles: '../www/css'
+};
+
+const rjsConfig = {
+    baseUrl: src.scripts,
+    mainConfigFile: src.scripts + '/require-config.js',
+    generateSourceMaps: false,
+
 };
 
 function getFolders(dir)
@@ -31,33 +37,21 @@ function getFolders(dir)
 
 gulp.task('scripts', () =>
 {
-    let folders = getFolders(src.scripts);
+    let folders = getFolders(src.views);
 
-    let tasks = folders.map((folder) =>
-        gulp.src(path.join(src.scripts, folder, '/**/*.js'))
+    return folders.map((folder) =>
+        gulp.src(path.join(src.views, folder, '/**/*.js'))
             .pipe(sourcemaps.init())
             .pipe(babel({
                 presets: ['es2015']
             }))
+            .pipe(rjs((file) => {
+                return Object.assign({include: path.join('views', folder, file.relative ), out: folder + '.js'}, rjsConfig);
+            }))
             .pipe(concat(folder + '.js'))
-            .pipe(uglify())
-            .pipe(rename(folder + '.min.js'))
             .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest(dist.scripts))
     );
-
-    // concat remaining files to main js file
-    let root = gulp.src(path.join(src.scripts, '/*.js'))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(uglify())
-        .pipe(rename('main.min.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(dist.scripts));
-
-    return merge(tasks, root);
 });
 
 gulp.task('less', () => {
@@ -90,7 +84,7 @@ gulp.task('requirejs-dependencies', () =>
 gulp.task('watch', () =>
 {
     gulp.watch(src.scripts + '/**/*.js', ['scripts']);
-    gulp.watch(src.scripts + '/**/*.js', ['requirejs-dependencies']);
+    //gulp.watch(src.scripts + '/**/*.js', ['requirejs-dependencies']);
 });
 
-gulp.task('default', ['watch', 'scripts', 'requirejs-dependencies']);
+gulp.task('default', ['watch', 'scripts']);
